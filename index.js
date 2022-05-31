8b04d8a2ab7a8f2aef6c03668ed6920b8afeebae
david <davidjhoyjr@gmail.com>
2022-05-31 11:19:57 -0400

    Added POST request

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ function createBrewery(breweries){
             document.querySelector('#state').textContent = brewery.state
 
         })
+        brewLog.append(brewPic)
 
     })
 }
@@ -38,16 +39,59 @@ function createBrewery(breweries){
 document.getElementById(`new-brewery`).addEventListener(`submit`,function(event){
     event.preventDefault()    //prevents the form from autosubmitting
 
-const newBrewery = document.createElement('img')
-newBrewery.src = event.target.image.value
-brewLog.append(newBrewery)
-    newBrewery.addEventListener(`click`, () =>{
-        document.querySelector(`.lrg-image`).src = event.target.image.value
-        document.querySelector(`.brewery-name`).textContent = event.target.name.value
-        document.querySelector(`#city`).textContent = event.target[`new-city`].value
-        document.querySelector(`#state`).textContent = event.target[`new-state`].value
+
+    //add brewery from the form to the "brewLog" list at the top of the page. 
+    const newBrewery = document.createElement('img')
+    newBrewery.src = event.target.image.value
+    brewLog.append(newBrewery)
+
+
+    // newBrewery.addEventListener(`click`, () =>{
+    //     // document.querySelector(`.lrg-image`).src = event.target.image.value
+    //     // document.querySelector(`.brewery-name`).textContent = event.target.name.value
+    //     // document.querySelector(`#city`).textContent = event.target[`new-city`].value
+    //     // document.querySelector(`#state`).textContent = event.target[`new-state`].value
 
 
 
-    })
+    // })
+
+    //replacing the above code with a POST Request 
+
+    //turning the link and actual body of the POST request into variables
+    const myUrl =   'http://localhost:3000/breweries' 
+    const myData = {
+        method: 'POST', 
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name: event.target.name.value,
+            image: event.target.image.value,
+            city: event.target[`new-city`].value,
+            state: event.target[`new-state`].value
+        }),
+      }
+      
+    
+
+    //the actual POST request using the variables above 
+    fetch(myUrl, myData)
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      })
+
+
+
+
+
+
+
+
+
+
 })
